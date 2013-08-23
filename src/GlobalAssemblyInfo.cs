4372675c2b98403cd2afc1b0ac7f78fbd903a6b3
David Muto <david.muto@gmail.com>
2013-08-23 14:45:47 -0400

    bumping version and generating dist package

---
+++ src/GlobalAssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MutoMark")]
+[assembly: AssemblyDescription("A MarkDown Viewer for Windows")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("David Muto")]
+[assembly: AssemblyProduct("MutoMark")]
+[assembly: AssemblyCopyright("Copyright © David Muto 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion(Version.NUMBER + ".*")]
+[assembly: AssemblyInformationalVersion(Version.NUMBER)]
+[assembly: AssemblyFileVersion(Version.NUMBER + ".0")]
+
+class Version
+{
+    public const string NUMBER = "0.1.1";
+}
--- src/MutoMark/Properties/AssemblyInfo.cs
+++ src/MutoMark/Properties/AssemblyInfo.cs
@@ -2,35 +2,5 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("MutoMark")]
-[assembly: AssemblyDescription("A MarkDown Viewer for Windows")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("David Muto")]
-[assembly: AssemblyProduct("MutoMark")]
-[assembly: AssemblyCopyright("Copyright © David Muto 2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ae6bf082-6270-482f-9257-44292c62f7d6")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.*")]
-[assembly: AssemblyFileVersion("0.1.0.0")]

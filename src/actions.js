3c90abdafab2f60ea876203b731362825e08d4ba
David Muto (pseudomuto) <david.muto@gmail.com>
2016-11-16 16:23:42 -0500

    Add fs-jetpack to promisify fs things

--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import { readFile } from 'fs'
+import { readAsync } from 'fs-jetpack'
 import { toHTML } from './markdown/index'
 
 export const CONTENT_CHANGED = 'CONTENT_CHANGED'
@@ -9,21 +9,9 @@ const updateContents = (html) => {
   return { type: CONTENT_CHANGED, html }
 }
 
-const readFileAsync = (path) => {
-  return new Promise((resolve, reject) => {
-    readFile(path, (error, content) => {
-      if (error) {
-        reject(error)
-      } else {
-        resolve(content)
-      }
-    })
-  })
-}
-
 export const convertFileToHTML = (path) => {
   return dispatch => {
-    return readFileAsync(path).then(contents => {
+    return readAsync(path).then(contents => {
       const md = contents.toString()
       return toHTML(md).then(html => dispatch(updateContents(html)))
     })
